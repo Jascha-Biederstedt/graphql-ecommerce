93c59b36207aa6a13823d8a279c6a9fb9a3c2fb3
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-05-11 15:41:48 +0200

    Add categories and implement querying by id

--- index.js
+++ index.js
@@ -84,13 +84,31 @@ const products = [
   },
 ];
 
+const categories = [
+  {
+    id: 'c01b1ff4-f894-4ef2-b27a-22aacc2fca70',
+    name: 'Kitchen',
+  },
+  {
+    id: '34115aac-0ff5-4859-8f43-10e8db23602b',
+    name: 'Garden',
+  },
+  {
+    id: 'd914aec0-25b2-4103-9ed8-225d39018d1d',
+    name: 'Sports',
+  },
+];
+
 const typeDefs = gql`
   type Query {
     products: [Product!]!
     product(id: ID!): Product
+    categories: [Category!]!
+    category(id: ID!): Category
   }
 
   type Product {
+    id: ID!
     name: String!
     description: String!
     quantity: Int!
@@ -98,20 +116,23 @@ const typeDefs = gql`
     image: String!
     onSale: Boolean!
   }
+
+  type Category {
+    id: ID!
+    name: String!
+    products: [Product!]
+  }
 `;
 
 const resolvers = {
   Query: {
-    products: () => {
-      return products;
-    },
-    product: (parent, args, context) => {
-      const productID = args.id;
+    products: () => products,
+    product: (parent, args, context) =>
+      products.find(product => product.id === args.id),
 
-      const product = products.find(product => product.id === productID);
-
-      return product || null;
-    },
+    categories: () => categories,
+    category: (parent, args, context) =>
+      categories.find(category => category.id === args.id),
   },
 };
 
