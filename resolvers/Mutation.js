0246d70bf6ed239b7b47472d2fc22771130a3d9d
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-05-12 15:37:06 +0200

    Add mutation for new category

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,3 +1,16 @@
+const { v4: uuid } = require('uuid');
+
 exports.Mutation = {
-  addCategory: (parent, { input }, context) => {},
+  addCategory: (parent, { input }, { categories }) => {
+    const { name } = input;
+
+    const newCategory = {
+      id: uuid(),
+      name,
+    };
+
+    categories.push(newCategory);
+
+    return newCategory;
+  },
 };
