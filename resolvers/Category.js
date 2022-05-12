d46cc47e72df2994e9ae6d40a13f784c66bb4609
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-05-12 15:13:17 +0200

    Implement querying by average rating

--- resolvers/Category.js
+++ resolvers/Category.js
@@ -1,6 +1,35 @@
 exports.Category = {
-  products: ({ id }, { filter }, { products }) =>
-    filter && filter.onSale
-      ? products.filter(product => product.categoryId === id && product.onSale)
-      : products.filter(product => product.categoryId === id),
+  products: ({ id: categoryId }, { filter }, { products, reviews }) => {
+    const categoryProducts = products.filter(
+      product => product.categoryId === categoryId
+    );
+
+    let filteredProducts = categoryProducts;
+
+    if (filter) {
+      if (filter.onSale === true) {
+        filteredProducts = products.filter(product => product.onSale);
+      }
+
+      if ([1, 2, 3, 4, 5].includes(filter.avgRating)) {
+        filteredProducts = filteredProducts.filter(product => {
+          let sumRating = 0;
+          let numberOfReviews = 0;
+
+          reviews.forEach(review => {
+            if (review.productId === product.id) {
+              sumRating += review.rating;
+              numberOfReviews++;
+            }
+          });
+
+          const avgProductRating = sumRating / numberOfReviews;
+
+          return avgProductRating >= filter.avgRating;
+        });
+      }
+    }
+
+    return filteredProducts;
+  },
 };
--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,8 +1,33 @@
 exports.Query = {
-  products: (parent, { filter }, { products }) =>
-    filter && filter.onSale
-      ? products.filter(product => product.onSale)
-      : products,
+  products: (parent, { filter }, { products, reviews }) => {
+    let filteredProducts = products;
+
+    if (filter) {
+      if (filter.onSale === true) {
+        filteredProducts = products.filter(product => product.onSale);
+      }
+
+      if ([1, 2, 3, 4, 5].includes(filter.avgRating)) {
+        filteredProducts = filteredProducts.filter(product => {
+          let sumRating = 0;
+          let numberOfReviews = 0;
+
+          reviews.forEach(review => {
+            if (review.productId === product.id) {
+              sumRating += review.rating;
+              numberOfReviews++;
+            }
+          });
+
+          const avgProductRating = sumRating / numberOfReviews;
+
+          return avgProductRating >= filter.avgRating;
+        });
+      }
+    }
+
+    return filteredProducts;
+  },
   product: (parent, { id }, { products }) =>
     products.find(product => product.id === id),
 
